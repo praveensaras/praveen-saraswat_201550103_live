9042d64caae66977530f429b7b30af0a010f0e43
praveensaras <109068418+praveensaras@users.noreply.github.com>
2022-07-11 22:49:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function myFunction()
+{
+    
+    document.getElementById("1").innerHTML = "praveen saraswat";
+    document.getElementById("2").innerHTML = "201550103";
+
+    document.getElementById("3").innerHTML =  "GLA University, Mathura";
+
+    document.getElementById("4").innerHTML = "11-07-2022  ";
+}
+
+
+
+
+
+
+
+     
+        // document.getElementById("1").innerHTML = "praveen saraswat";
+        // document.getElementById("2").innerHTML = "201550103";
+
+        // document.getElementById("3").innerHTML =  "GLA University, Mathura";
+
+        // document.getElementById("4").innerHTML = "11-07-2022  ";
+
+
+        
+       
